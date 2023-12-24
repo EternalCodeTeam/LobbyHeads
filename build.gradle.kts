b815c894bf2345c9a9801c29a5546e8e283373d6
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-12-24 13:45:48 +0100

    Revert checkstyle version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ repositories {
 }
 
 checkstyle {
-    toolVersion = "10.12.6"
+    toolVersion = "10.12.4"
 
     configFile = file("${rootDir}/checkstyle/checkstyle.xml")
     configProperties["checkstyle.suppressions.file"] = "${rootDir}/checkstyle/suppressions.xml"
